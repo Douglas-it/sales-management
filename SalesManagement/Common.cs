369591d482f7f1e75fabb95e486d203a4bd85488
Ferreira9006 <ferreirawow@gmail.com>
2024-06-09 20:37:44 +0100

    Create Common.cs
    Common.cs is the file where general functions will be coded at.

---
+++ SalesManagement/Common.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class OperacoesGerais
+{
+    // Função para ler uma string não vazia
+    public static bool LerStringValida(string valor)
+    {
+        if (string.IsNullOrWhiteSpace(valor))
+            return false;
+
+        return true;
+    }
+
+    // Função para ler um inteiro válido do console dentro do intervalo especificado
+    public static bool LerInteiroValido(string entrada, int valorMinimo = int.MinValue, int valorMaximo = int.MaxValue)
+    {
+        int saida;
+        bool valido = int.TryParse(entrada, out saida) && saida >= valorMinimo && saida <= valorMaximo;
+        return valido;
+    }
+
+    // Lê um número decimal válido da consola dentro do intervalo especificado, com mensagem de prompt opcional
+    public static bool LerDecimalValido(string entrada, float valorMinimo = float.MinValue, float valorMaximo = float.MaxValue)
+    {
+        float saida;
+        bool valido = float.TryParse(entrada, out saida) && saida >= valorMinimo && saida <= valorMaximo;
+        return valido;
+    }
+}
