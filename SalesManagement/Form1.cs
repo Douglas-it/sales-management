24b0065d458b60c9c7c5be34cedc9db5bb924593
Ferreira9006 <ferreirawow@gmail.com>
2024-06-10 10:12:39 +0100

    New class - Users (Will handle all the users needs)

--- SalesManagement/Form1.cs
+++ SalesManagement/Form1.cs
@@ -20,38 +20,9 @@ namespace SalesManagement
             // Verificar se o username e password são válidos
             if (OperacoesGerais.LerStringValida(username) && OperacoesGerais.LerStringValida(password))
             {
-                try
-                {
-                    // Inicializar a classe DatabaseHelper
-                    DatabaseHelper dbHelper = new DatabaseHelper();
 
-                    // Query para selecionar o utilizador
-                    string selectQuery = "SELECT * FROM Utilizadores WHERE Utilizador = @Username AND Senha= @Password";
-
-                    // Parâmetros para a query
-                    SqlParameter param1 = new SqlParameter("@Username", SqlDbType.VarChar) { Value = username };
-                    SqlParameter param2 = new SqlParameter("@Password", SqlDbType.VarChar) { Value = password };
-
-
-                    // Obter o resultado da query
-                    DataTable result = dbHelper.GetDataTable(selectQuery, param1, param2);
-
-                    // Se o resultado tiver 1 linha, então o utilizador existe
-                    if (result.Rows.Count == 1)
-                    {
-                        FormInicial FormInicial = new FormInicial(); // Inicializar novo form
-                        FormInicial.Show(); // Mostra Novo Form
-
-                        this.Hide(); // Esconde o Form de Login
-                    }
-                    else
-                        MessageBox.Show("Credenciais inválidas!", "Erro!", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
-                catch (Exception ex) // Apanhar exceções
-                {
-                    MessageBox.Show("Erro ao tentar conectar a base de dados: " + ex.Message);
-                }
-                
+                if (Utilizadores.login(username, password)) // Chamar a função login da classe Utilizadores
+                    this.Hide(); // Esconder o form atual
             }
             else
                 MessageBox.Show("O formato das credências esta inválido!"); // Mostrar mensagem de erro
