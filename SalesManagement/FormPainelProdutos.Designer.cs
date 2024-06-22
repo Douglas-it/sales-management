a85db58020131baf355096d950920f504864aaec
Douglas-it <maicon.santos.eng@outlook.com>
2024-06-23 00:42:23 +0100

    Icons update

--- SalesManagement/FormPainelProdutos.Designer.cs
+++ SalesManagement/FormPainelProdutos.Designer.cs
@@ -40,36 +40,44 @@
             // 
             // btnLimpar
             // 
-            btnLimpar.Location = new Point(732, 76);
+            btnLimpar.Cursor = Cursors.Hand;
+            btnLimpar.Location = new Point(837, 101);
+            btnLimpar.Margin = new Padding(3, 4, 3, 4);
             btnLimpar.Name = "btnLimpar";
-            btnLimpar.Size = new Size(56, 26);
+            btnLimpar.Size = new Size(64, 35);
             btnLimpar.TabIndex = 22;
             btnLimpar.Text = "Limpar";
             btnLimpar.UseVisualStyleBackColor = true;
             // 
             // inputPesquisa
             // 
-            inputPesquisa.Location = new Point(354, 79);
+            inputPesquisa.Cursor = Cursors.IBeam;
+            inputPesquisa.Location = new Point(405, 105);
+            inputPesquisa.Margin = new Padding(3, 4, 3, 4);
             inputPesquisa.Name = "inputPesquisa";
             inputPesquisa.PlaceholderText = "Pesquise por nome ou código ou categoria";
-            inputPesquisa.Size = new Size(257, 23);
+            inputPesquisa.Size = new Size(293, 27);
             inputPesquisa.TabIndex = 16;
             inputPesquisa.TextChanged += inputPesquisa_TextChanged;
             // 
             // btnPesquisar
             // 
-            btnPesquisar.Location = new Point(651, 76);
+            btnPesquisar.Cursor = Cursors.Hand;
+            btnPesquisar.Location = new Point(744, 101);
+            btnPesquisar.Margin = new Padding(3, 4, 3, 4);
             btnPesquisar.Name = "btnPesquisar";
-            btnPesquisar.Size = new Size(75, 26);
+            btnPesquisar.Size = new Size(86, 35);
             btnPesquisar.TabIndex = 21;
             btnPesquisar.Text = "Pesquisar";
             btnPesquisar.UseVisualStyleBackColor = true;
             // 
             // btnSair
             // 
-            btnSair.Location = new Point(713, 10);
+            btnSair.Cursor = Cursors.Hand;
+            btnSair.Location = new Point(815, 13);
+            btnSair.Margin = new Padding(3, 4, 3, 4);
             btnSair.Name = "btnSair";
-            btnSair.Size = new Size(75, 23);
+            btnSair.Size = new Size(86, 31);
             btnSair.TabIndex = 20;
             btnSair.Text = "Sair";
             btnSair.UseVisualStyleBackColor = true;
@@ -78,26 +86,31 @@
             // ListaProdutos
             // 
             ListaProdutos.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            ListaProdutos.Location = new Point(12, 107);
+            ListaProdutos.Location = new Point(14, 143);
+            ListaProdutos.Margin = new Padding(3, 4, 3, 4);
             ListaProdutos.Name = "ListaProdutos";
             ListaProdutos.RowHeadersWidth = 51;
-            ListaProdutos.Size = new Size(776, 332);
+            ListaProdutos.Size = new Size(887, 443);
             ListaProdutos.TabIndex = 19;
             // 
             // btnNovoProduto
             // 
-            btnNovoProduto.Location = new Point(12, 76);
+            btnNovoProduto.Cursor = Cursors.Hand;
+            btnNovoProduto.Location = new Point(14, 101);
+            btnNovoProduto.Margin = new Padding(3, 4, 3, 4);
             btnNovoProduto.Name = "btnNovoProduto";
-            btnNovoProduto.Size = new Size(136, 23);
+            btnNovoProduto.Size = new Size(155, 31);
             btnNovoProduto.TabIndex = 18;
             btnNovoProduto.Text = "Novo Produto";
             btnNovoProduto.UseVisualStyleBackColor = true;
             // 
             // btnVoltar
             // 
-            btnVoltar.Location = new Point(12, 10);
+            btnVoltar.Cursor = Cursors.Hand;
+            btnVoltar.Location = new Point(14, 13);
+            btnVoltar.Margin = new Padding(3, 4, 3, 4);
             btnVoltar.Name = "btnVoltar";
-            btnVoltar.Size = new Size(75, 23);
+            btnVoltar.Size = new Size(86, 31);
             btnVoltar.TabIndex = 17;
             btnVoltar.Text = "Voltar";
             btnVoltar.UseVisualStyleBackColor = true;
@@ -105,9 +118,9 @@
             // 
             // FormProdutos
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(914, 600);
             Controls.Add(btnLimpar);
             Controls.Add(inputPesquisa);
             Controls.Add(btnPesquisar);
@@ -115,6 +128,7 @@
             Controls.Add(ListaProdutos);
             Controls.Add(btnNovoProduto);
             Controls.Add(btnVoltar);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "FormProdutos";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Painel de Produtos";
